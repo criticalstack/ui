9bb8af1b766701fb0a6807d15a76f9c11db175de
YenHernandez <yen.a@me.com>
2020-12-01 11:01:12 -0500

    Nivo-line

--- client/lib/datacenter/containers/container-swoll.jsx
+++ client/lib/datacenter/containers/container-swoll.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Chart from "../../../shared/charts/chart";
-// import { ResponsiveSankey } from "@nivo/sankey";
+import { ResponsiveLine } from '@nivo/line'
 
 class ContainerSwoll extends React.Component {
 
@@ -26,20 +25,70 @@ class ContainerSwoll extends React.Component {
     console.log(swTotalRateUrl);
     return (
       <div className="container-chart-parent">
-        <Chart
-          name="swoll"
-          title="Swoll Metrics"
-          parentClass="chart-container"
-          fakeData={false}
-          url={swTotalRateUrl}
-          lineColor="#58a757"
-          fillColor="rgba(136, 214, 136, 0.27)"
-          dotColor="#58a757"
-          dotHoverColor="#58a757"
-          yAxisLabel="Cores"
-          modifier={(x) => parseFloat(x * 0.001)}
-        />
-      </div>
+        <ResponsiveLine
+        data = {data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'timestamp',
+          legendOffset: 36,
+          legendPosition: 'middle'
+        }}
+        axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'value',
+          legendOffset: -40,
+          legendPosition: 'middle'
+        }}
+        colors={{ scheme: 'dark2' }}
+        lineWidth={5}
+        pointSize={10}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabelYOffset={-12}
+        areaBaselineValue={30}
+        areaOpacity={0.3}
+        useMesh={true}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
+        </div>
     );
   }
 }
